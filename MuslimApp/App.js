e49a7c3fe11690a2d6fa84c047d35a2249da0277
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-03-18 18:05:00 +0700

    [EDIT] Create navigator

--- MuslimApp/App.js
+++ MuslimApp/App.js
@@ -4,31 +4,93 @@
  *
  * @format
  * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View} from 'react-native';
+import restaurant from './screen/restaurant';
+import prayerPlace from './screen/prayerPlace';
+import prayerTime from './screen/prayerTime';
+import account from './screen/account';
+import {createBottomTabNavigator,createAppContainer} from 'react-navigation';
+import {Icon} from 'native-base';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.welcome}>Welcome to my Application!</Text>
       </View>
     );
   }
 }
 
+const TabNavigator = createBottomTabNavigator(
+  {
+    Home: {screen: App,
+      navigationOptions:{
+      tabBarIcon:()=>(
+        <Icon name="home" style={{color:'white'}} size={20}/>
+      )
+    }
+  },
+    Restaurant:{screen: restaurant,
+      navigationOptions:{
+        tabBarIcon:()=>(
+          <Icon name="md-restaurant" style={{color:'white'}} size={20}/>
+        )
+      }
+    },
+    PrayerPlace:{screen: prayerPlace,
+      navigationOptions:{
+        tabBarIcon:()=>(
+          <Icon name="md-restaurant" style={{color:'white'}} size={20}/>
+        )
+      }
+    },
+    PrayerTime:{screen: prayerTime,
+      navigationOptions:{
+        tabBarIcon:()=>(
+          <Icon name="ios-time" style={{color:'white'}} size={20}/>
+        )
+      }
+    },
+    Account:{screen: account,
+      navigationOptions:{
+        tabBarIcon:()=>(
+          <Icon name="md-restaurant" style={{color:'white'}} size={20}/>
+        )
+      }
+    },
+  },
+  {
+    tabBarOptions: {
+      style: {
+           height:60,
+          backgroundColor: '#CC6600',
+          padding: 8,
+      },
+       indicatorStyle: {
+          borderBottomColor: '#ffffff',
+          borderBottomWidth: 3,
+      },
+      tabStyle: {
+          borderColor: '#CC6600',
+          borderRightWidth: 1,
+      },
+      labelStyle: {
+        fontSize: 12,
+         marginTop: 0,
+         color :'#ffffff'
+      },
+    }
+  },
+    {
+      initialRouteName : 'Home'
+    }
+);
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -40,10 +102,8 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+    color: 'orange'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+}
+);
+export default createAppContainer(TabNavigator);
