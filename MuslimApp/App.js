9fafc195ae5ddba9b82ecb185f9ca020161d85ce
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-03-19 23:55:19 +0700

    [ADD] Stack Navigator

--- MuslimApp/App.js
+++ MuslimApp/App.js
@@ -1,20 +1,15 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
-
 import React, {Component} from 'react';
 import { StyleSheet, Text, View} from 'react-native';
 import restaurant from './screen/restaurant';
 import prayerPlace from './screen/prayerPlace';
 import prayerTime from './screen/prayerTime';
 import account from './screen/account';
-import {createBottomTabNavigator,createAppContainer} from 'react-navigation';
-import {Icon} from 'native-base';
+import {createBottomTabNavigator,createAppContainer, createStackNavigator} from 'react-navigation';
+import Icon from 'react-native-vector-icons/Ionicons';
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
+import showAllRestaurant from './screen/showAllRestaurant';
+import { Button } from 'native-base';
+import detail from './screen/detail';
 
 
 class App extends Component {
@@ -22,45 +17,78 @@ class App extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to my Application!</Text>
+        <Button onPress={() => this.props.navigation.navigate('ShowAll')}>
+          <Text>View All</Text>
+          </Button>
       </View>
     );
   }
 }
-
+const StackNavigator2 = createStackNavigator(
+  {
+    Navigate2:{ screen: App,
+     navigationOptions:{
+       title:"Home"
+     }},
+    ShowAll:{ screen: showAllRestaurant,
+      navigationOptions:{
+        title:"All restaurant"
+      }},
+    detail:{ screen: detail,
+      navigationOptions:{
+        title:"Detail"
+      }}
+  },
+  {
+    initialRouteName : 'Navigate2',
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#CC6600',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: { 
+        flex:1,
+        textAlign: 'center',
+        alignSelf: 'center' 
+      },
+      headerRight: (<View />)
+    },
+  }
+);
 const TabNavigator = createBottomTabNavigator(
   {
-    Home: {screen: App,
+    Home: {screen: StackNavigator2,
       navigationOptions:{
       tabBarIcon:()=>(
-        <Icon name="home" style={{color:'white'}} size={20}/>
+        <Icon name="ios-home" style={{color:'white'}} size={30}/>
       )
     }
   },
     Restaurant:{screen: restaurant,
       navigationOptions:{
         tabBarIcon:()=>(
-          <Icon name="md-restaurant" style={{color:'white'}} size={20}/>
+          <Icon name="md-restaurant" style={{color:'white'}} size={30}/>
         )
       }
     },
     PrayerPlace:{screen: prayerPlace,
       navigationOptions:{
         tabBarIcon:()=>(
-          <Icon name="md-restaurant" style={{color:'white'}} size={20}/>
+          <Icons name="home-map-marker" style={{color:'white'}} size={30}/>
         )
       }
     },
     PrayerTime:{screen: prayerTime,
       navigationOptions:{
         tabBarIcon:()=>(
-          <Icon name="ios-time" style={{color:'white'}} size={20}/>
+          <Icon name="ios-alarm" style={{color:'white'}} size={30}/>
         )
       }
     },
     Account:{screen: account,
       navigationOptions:{
         tabBarIcon:()=>(
-          <Icon name="md-restaurant" style={{color:'white'}} size={20}/>
+          <Icon name="ios-contact" style={{color:'white'}} size={30}/>
         )
       }
     },
