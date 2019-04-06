944d904e91a5986a51cf0cf927696300e9f318e5
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-04-07 00:15:42 +0700

    [ADD] Create constructor to get parameter

--- MuslimApp/screen/detail.js
+++ MuslimApp/screen/detail.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Image} from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
 export default class detail extends Component {
 
   render() {
+    const { navigation } = this.props;
+    const title = navigation.getParam('titleId');
+    const artist = navigation.getParam('artist');
+    const thumbnail = navigation.getParam('thumbnail');
+    const image = navigation.getParam('image');
     return (
-        <View style={styles.container}>
-        <Text style={{color: 'white'}}> Detail </Text>
-      </View>
+        <Card>
+          <CardSection>
+            <View>
+            <Image style={styles.thumbnailStyle} source={{ uri: thumbnail }}/>
+            </View>
+            <View style={styles.headerContentStyle}>
+              <Text>Title: {JSON.stringify(title)}</Text>
+              <Text>Artist: {JSON.stringify(artist)}</Text>
+            </View>
+            </CardSection>
+            <CardSection>
+            <Image style={styles.imageStyle} source={{ uri: image }}/>
+          </CardSection>
+        </Card>
     );
   }
 }
-const styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor:'red'
-    }
-});
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    marginLeft: 10
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
+  }
+};
