451002e553cfe85dbd03fa1c489cf57c7bbea5f3
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-05-26 18:06:13 +0700

    [EDIT] Add Header Span

--- MuslimApp/screen/account.js
+++ MuslimApp/screen/account.js
@@ -1,21 +1,32 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Container, Header, Title, Button, Icon, Left, Right, Body } from "native-base";
 
 export default class account extends Component {
   render() {
     return (
-        <View style={styles.container}>
-          <Text style={{color: 'white'}}> Account </Text>
-        </View>
+      <Container>
+      <Header span style={styles.container}>
+        <Left>
+          <Button transparent>
+            <Icon name="arrow-back" />
+          </Button>
+        </Left>
+        <Body>
+          <Title>Header Span</Title>
+        </Body>
+        <Right />
+      </Header>
+    </Container>
       );
     }
   }
   
   const styles = StyleSheet.create({
       container:{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-          backgroundColor:'yellow'
+          // flex: 1,
+          // justifyContent: 'center',
+          // alignItems: 'center',
+          backgroundColor:'#CC6600'
       }
   });
