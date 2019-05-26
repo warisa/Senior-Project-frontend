87cacd6a055313c914b2d6309fbb0c10f2e56bc4
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-05-26 15:48:45 +0700

    [ADD] Add method search

--- MuslimApp/screen/restaurantDetail.js
+++ MuslimApp/screen/restaurantDetail.js
@@ -6,6 +6,8 @@ import { ScrollView } from 'react-native-gesture-handler';
 import {Button, Icon,Header,Left,Right,Body, Container,Title} from 'native-base';
 import MapApp from '../component/MapApp';
 import Axios from 'axios';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
 
 export default class restaurantDetail extends Component {
 
@@ -13,13 +15,14 @@ export default class restaurantDetail extends Component {
     super(props)
       this.state = { //ประกาศตัวแปรใน this.state นอกstate = ค่าคงที่
         place: [],
+        image:[],
         placeId: props.navigation.getParam('placeId')
       }
     }
 
   componentWillMount() {
     Axios.get('http://10.4.56.94/restaurant/'+ this.state.placeId)
-    .then(response => this.setState({ place: response.data[0] }))
+    .then(response => this.setState({ place: response.data[0], image: response.data }))
   }
 
   render() {
@@ -41,35 +44,39 @@ export default class restaurantDetail extends Component {
         </Header> */}
         <ScrollView>
         <Card>
-          <Image source={{uri:'http://halalinthailand.com/wp-content/uploads/2016/03/IMG_2108.jpg'}}
+          <Image source={{uri:this.state.place.imageName}}
                   style={{width: '100%', height: 200}}/>
           <CardSection>
             <View >
               <View style={{borderRadius:30,backgroundColor:'#FFDAB9'}}>
                 <Text style={{color:'black',fontSize:20,fontWeight:'bold'}}> Title: {this.state.place.placeName}</Text>
               </View>
+                  {/* <ScrollView horizontal={true} style={{flexDirection:'row'}} 
+                    showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
+                         {
+                            this.state.image.map( image => 
+                            <View key={image.imageId} style={{alignItems: 'center', marginTop:10, width:130,height:150}}>
+                                  <Image source={{uri: image.imageName}} style={{width: 120, height: 100, margin: 7}} />
+                            </View>
+                            )
+                          }      
+                  </ScrollView> */}
               </View>
             </CardSection>
             <CardSection>
-                {/* <View>
+                <View>
                     <Text style={{color:'black',marginBottom:10}}>{this.state.place.placeDescription}</Text>
                     <ScrollView horizontal={true} style={{flexDirection:'row'}} 
                     showsHorizontalScrollIndicator={false} showsVerticalScrollIndicator={false}>
-                        <Image style={styles.image} source={{uri:'http://halalinthailand.com/wp-content/uploads/2016/03/IMG_2166.jpg'}} />
-                        <Image style={styles.image} source=
-                        {{uri:'https://scontent.fbkk5-4.fna.fbcdn.net/v/t1.0-9/56523198_2294051060915296_4404012001634811904_o.jpg?_nc_cat=103&_nc_oc=AQmP1y9pnyScnbX5zFLNszuOISubil7YRsnpBxOiOSsDRIC0xQQi8IbqDcWsTo_r64UbFyldsoiVc7cmM3E6Icn-&_nc_ht=scontent.fbkk5-4.fna&oh=91ffd99b43b39f8e8115588257c86d2e&oe=5D3CFF81'}} />
-                        <Image style={styles.image} source=
-                        {{uri:'https://scontent.fbkk5-8.fna.fbcdn.net/v/t1.0-9/52875380_2263569720630097_7129378289516281856_o.jpg?_nc_cat=106&_nc_oc=AQm42IqQlnSumllxJ59qyjE7DhURN1ZBDGO_EGcWD6q6B3tMHU5Jc156fmcor8yeqQuZKa9L1QWi8Iz5KzHAkWMj&_nc_ht=scontent.fbkk5-8.fna&oh=f7be189819fde204e8dc2d9c4e944e75&oe=5D37FC9B'}}/>
-                        <Image style={styles.image} source=
-                        {{uri:'https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/52591424_2263569710630098_5790794479803826176_n.jpg?_nc_cat=105&_nc_oc=AQkSjkbw_sHk_ZrIRK_3biJxx7rVGjLAdUH3C0K8bKol8pqcklXfS95OH4tQPVg5vUwUAGGcyJeRVbWcVs7fnBGm&_nc_ht=scontent.fbkk5-3.fna&oh=7e1cb5767b01a73bbb987bdc7c6926c1&oe=5D4792E3'}}/>
-                        <Image style={styles.image} source=
-                        {{uri:'https://scontent.fbkk5-3.fna.fbcdn.net/v/t1.0-9/52605737_2263569673963435_7779312184063950848_n.jpg?_nc_cat=105&_nc_oc=AQknrLX6xUOoGzRSt324fMIhd_69a3zHhJHsLMrwcMlfIsD4KthwumTEy-ZAft1iHsSD04yzXoS_Zl6MTSfsTwjQ&_nc_ht=scontent.fbkk5-3.fna&oh=3f6d8458d22a23d7019836613c1581af&oe=5D03F207'}}/>
-                    </ScrollView>
-                </View>   */}
-            {/* <View style={styles.headerContentStyle}>
-              <Text>Title: {JSON.stringify(title)}</Text>
-              <Text>Artist: {JSON.stringify(artist)}</Text>
-            </View> */}
+                         {
+                            this.state.image.map( image => 
+                            <View key={image.imageId} style={{alignItems: 'center', marginTop:10, width:130,height:150}}>
+                                  <Image source={{uri: image.imageName}} style={{width: 120, height: 100, margin: 7}} />
+                            </View>
+                            )
+                          }      
+                  </ScrollView>
+                </View>  
             </CardSection>
             <CardSection>
               <View>
@@ -91,20 +98,100 @@ export default class restaurantDetail extends Component {
               </View>
             {/* <Image style={styles.imageStyle} source={{ uri: image }}/> */}
             </CardSection>
-            <MapApp/>
+            <MapApp 
+              placeName={this.state.place.placeName}
+              latitude={this.state.place.latitude} 
+              longitude={this.state.place.longtitude}
+            />
             <Text style={{color:'black',fontSize:17,fontWeight:'bold',marginTop:5}}>รายละเอียดร้านเพิ่มเติม</Text>
             <Card>
               <CardSection>
                 <View style={{margin:10}}>
-                  <Text style={styles.fontStyle2}> - ที่จอดรถ </Text>
-                  <Text style={styles.fontStyle2}> - ห้องละหมาด </Text>
+                {
+                  this.state.place.placeCarParking == 1 ?
+                  (
+                    <CardSection>
+                      <AntDesign name="checkcircleo" style={{color:'green'}} size={20}/>
+                      <Text style={styles.fontStyle3}>ที่จอดรถ </Text>
+                    </CardSection>
+                  )
+                  :
+                  (
+                    <CardSection>
+                      <Feather name="x-circle" style={{color:'red'}} size={20}/>
+                      <Text style={styles.fontStyle4}>ที่จอดรถ </Text>
+                    </CardSection>
+                  )
+                }
+
+                {
+                  this.state.place.placePrayerRoom == 1 ?
+                  (
+                    <CardSection>
+                      <AntDesign name="checkcircleo" style={{color:'green'}} size={19}/>
+                      <Text style={styles.fontStyle3}>ห้องละหมาด</Text>
+                    </CardSection>
+                  )
+                  :
+                  (
+                    <CardSection>
+                      <Feather name="x-circle" style={{color:'red'}} size={20}/>
+                      <Text style={styles.fontStyle4}>ห้องละหมาด</Text>
+                    </CardSection>
+                  )
+                }
                 </View>
                 <View style={{margin:10}}>
-                  <Text style={styles.fontStyle2}> - เครื่องปรับอากาศ </Text>
-                  <Text style={styles.fontStyle2}> - รับบัตรเครดิต </Text>
+                 {
+                  this.state.place.placeAirconditioner == 1 ?
+                  (
+                    <CardSection>
+                      <AntDesign name="checkcircleo" style={{color:'green'}} size={19}/>
+                      <Text style={styles.fontStyle3}>เครื่องปรับอากาศ </Text>
+                    </CardSection>
+                  )
+                  :
+                  (
+                    <CardSection>
+                      <Feather name="x-circle" style={{color:'red'}} size={20}/>
+                      <Text style={styles.fontStyle4}>เครื่องปรับอากาศ </Text>
+                    </CardSection>
+                  )
+                }
+                {
+                  this.state.place.placeCreditCard == 1 ?
+                  (
+                    <CardSection>
+                      <AntDesign name="checkcircleo" style={{color:'green'}} size={19}/>
+                      <Text style={styles.fontStyle3}> - รับบัตรเครดิต </Text>
+                    </CardSection>
+                  )
+                  :
+                  (
+                    <CardSection>
+                      <Feather name="x-circle" style={{color:'red'}} size={20}/>
+                      <Text style={styles.fontStyle4}>รับบัตรเครดิต </Text>
+                    </CardSection>
+                  )
+                }
                 </View>
                 <View style={{margin:10}}>
-                  <Text style={styles.fontStyle2}> - จองล่วงหน้า </Text>
+                {
+                  this.state.place.placeReserve == 1 ?
+                  (
+                    <CardSection>
+                      <AntDesign name="checkcircleo" style={{color:'green'}} size={19}/>
+                      <Text style={styles.fontStyle3}>จองล่วงหน้า </Text>
+                    </CardSection>
+                  )
+                  :
+                  (
+                    <CardSection>
+                      <Feather name="x-circle" style={{color:'red'}} size={20}/>
+                      <Text style={styles.fontStyle4}>จองล่วงหน้า </Text>
+                    </CardSection>
+                  )
+                }
                 </View>
               </CardSection>
             </Card>
@@ -143,6 +230,16 @@ const styles = {
   fontStyle2:{
     marginTop: 10,
     color: 'black',
+    fontSize: 12
+  },
+  fontStyle3:{
+    marginTop: 10,
+    color: 'green',
+    fontSize: 12
+  },
+  fontStyle4:{
+    marginTop: 10,
+    color: 'red',
     fontSize: 12
   },
   image:{
