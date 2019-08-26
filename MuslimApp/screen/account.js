d315afa04aa3bd6739daefa3368f0929a9f55d05
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-08-26 16:26:15 +0700

    [ADD] Add button login Facebook

--- MuslimApp/screen/account.js
+++ MuslimApp/screen/account.js
@@ -1,32 +1,42 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Container, Header, Title, Button, Icon, Left, Right, Body } from "native-base";
+import { View, Text, StyleSheet,Image } from 'react-native';
+import { Container, Header, Title, Button, Icon, Left, Right, Body, Content } from "native-base";
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
 export default class account extends Component {
   render() {
     return (
-      <Container>
-      <Header span style={styles.container}>
+  
+      <View style={styles.container}>
+        <Body>
+            <Image source={require('../image/Halal.png')} style={{width:'100%',height:150}} />
+            <Button block style={{marginTop:60}}>
+              <Text style={{color:'white',marginLeft:10}}>Login with facebook  </Text>
+            </Button>
+        </Body>
+      {/* <Header span style={styles.container}>
         <Left>
-          <Button transparent>
-            <Icon name="arrow-back" />
-          </Button>
+          
         </Left>
         <Body>
-          <Title>Header Span</Title>
+          <FontAwesome name='user-circle-o' style={{color:'white'}} size={30}/>
         </Body>
-        <Right />
-      </Header>
-    </Container>
+        <Right>
+        <Button bordered style={{marginTop:60}}>
+                  <Text style={{color:'white',marginLeft:10}}>Log in facebook</Text>
+                </Button>
+        </Right>
+      </Header> */}
+    </View>
       );
     }
   }
   
   const styles = StyleSheet.create({
       container:{
-          // flex: 1,
-          // justifyContent: 'center',
-          // alignItems: 'center',
+          flex: 1,
+          justifyContent: 'center',
+          alignItems: 'center',
           backgroundColor:'#CC6600'
       }
   });
