b1e21d99a43401a61fe26898e6cd3e6a66b0687f
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-09-10 19:33:55 +0700

    [EDIT] Add code notification

--- MuslimApp/App.js
+++ MuslimApp/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
+import firebase from 'react-native-firebase';
+import { StyleSheet, Text, View, Image, TouchableHighlight, Alert, AsyncStorage } from 'react-native';
 import restaurant from './screen/restaurant';
 import prayPlace from './screen/prayPlace';
 import prayTime from './screen/prayTime';
@@ -31,12 +32,115 @@ class App extends Component {
     }
     
   componentWillMount() {
+    this.setNotification()
     this.setState({ test : 'nut'})
     Axios.get('http://10.4.56.94/restaurant')
     .then(response => this.setState({ albums: response.data }))
     Axios.get('http://10.4.56.94/prayerplace')
     .then(response => this.setState({ pray: response.data }))
   }
+
+  componentWillUnmount() {
+    this.notificationListener;
+    this.notificationOpenedListener;
+  }
+
+  setNotification(){
+    this.checkPermission();
+    this.createNotificationListeners();
+  }
+
+  //1
+  async checkPermission() {
+    const enabled = await firebase.messaging().hasPermission();
+    if (enabled) {
+      this.getToken();
+    } else {
+      this.requestPermission();
+    }
+  }
+
+  //2
+  async requestPermission() {
+    try {
+      await firebase.messaging().requestPermission();
+      this.getToken();
+    } catch (error) {
+      console.log('permission rejected');
+    }
+  }
+
+  //3
+  async getToken() {
+    let fcmToken = await AsyncStorage.getItem('fcmToken');
+    if (!fcmToken) {
+      fcmToken = await firebase.messaging().getToken();
+      if (fcmToken) {
+        console.log('fcmToken:', fcmToken);
+        await AsyncStorage.setItem('fcmToken', fcmToken);
+      }
+    }
+    console.log('fcmToken:', fcmToken);
+  }
+
+  async createNotificationListeners() {
+    /*
+    * Triggered when a particular notification has been received in foreground
+    * */
+    this.notificationListener = firebase.notifications().onNotification((notification) => {
+      const { title, body } = notification;
+      console.log('onNotification:');
+      
+        const localNotification = new firebase.notifications.Notification({
+          sound: 'sampleaudio',
+          show_in_foreground: true,
+        })
+        .setSound('sampleaudio.wav')
+        .setNotificationId(notification.notificationId)
+        .setTitle(notification.title)
+        .setBody(notification.body)
+        .android.setChannelId('fcm_FirebaseNotifiction_default_channel') // e.g. the id you chose above
+        .android.setSmallIcon('@drawable/ic_launcher') // create this icon in Android Studio
+        .android.setColor('#000000') // you can set a color here
+        .android.setPriority(firebase.notifications.Android.Priority.High);
+
+        firebase.notifications()
+          .displayNotification(localNotification)
+          .catch(err => console.error(err));
+    });
+
+    const channel = new firebase.notifications.Android.Channel('fcm_FirebaseNotifiction_default_channel', 'Demo app name', firebase.notifications.Android.Importance.High)
+      .setDescription('Demo app description')
+      .setSound('sampleaudio.wav');
+    firebase.notifications().android.createChannel(channel);
+
+    /*
+    * If your app is in background, you can listen for when a notification is clicked / tapped / opened as follows:
+    * */
+    this.notificationOpenedListener = firebase.notifications().onNotificationOpened((notificationOpen) => {
+      const { title, body } = notificationOpen.notification;
+      console.log('onNotificationOpened:');
+      Alert.alert(title, body)
+    });
+
+    /*
+    * If your app is closed, you can check if it was opened by a notification being clicked / tapped / opened as follows:
+    * */
+    const notificationOpen = await firebase.notifications().getInitialNotification();
+    if (notificationOpen) {
+      const { title, body } = notificationOpen.notification;
+      console.log('getInitialNotification:');
+      Alert.alert(title, body)
+    }
+    /*
+    * Triggered for data only payload in foreground
+    * */
+    this.messageListener = firebase.messaging().onMessage((message) => {
+      //process data message
+      console.log("JSON.stringify:", JSON.stringify(message));
+    });
+  }
+
   render() {
     return (
       <Content>
--- MuslimApp/screen/prayTime.js
+++ MuslimApp/screen/prayTime.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import CardSection from './CardSection';
-import Card from './Card'
 import { Container, Content, Header, Title, List, ListItem, Left, Body, Right, Switch} from 'native-base';
 import Axios from 'axios';
-import { ScrollView } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/Ionicons';
 import IconEntypo from 'react-native-vector-icons/Entypo';
 
@@ -16,73 +13,75 @@ export default class prayTime extends Component {
         time: [],
       }
     }
+
     componentDidMount() {
       var that = this;
       var date = new Date().getDate(); //Current Date
       var month = new Date().getMonth() + 1; //Current Month
       var year = new Date().getFullYear(); //Current Year
       that.setState({
-        //Setting the value of the date time
         date:
           date + '/' + month + '/' + year,
       });
     }
+
     componentWillMount() {
       Axios.get('http://10.4.56.94/prayertime')
       .then(response => this.setState({ time: response.data }))
     }
-  render() {
-        return (
-          <Container>
-          <Header style={{backgroundColor: '#CC6600'}}>
-          <Body>
-            <Title>Pray Time</Title>
-          </Body>
-        </Header>
-        <Content>
-          <View>
-            <View style={{alignItems:'center',justifyContent:'center'}}>
-              <ListItem>
-                <IconEntypo name="calendar" style={{color:'black',marginRight:10}} size={30}/>
-                <Text style={styles.fontStyle}>{this.state.date}</Text>
-              </ListItem>
+
+    render() {
+          return (
+            <Container>
+            <Header style={{backgroundColor: '#CC6600'}}>
+            <Body>
+              <Title>Pray Time</Title>
+            </Body>
+          </Header>
+          <Content>
+            <View>
+              <View style={{alignItems:'center',justifyContent:'center'}}>
+                <ListItem>
+                  <IconEntypo name="calendar" style={{color:'black',marginRight:10}} size={30}/>
+                  <Text style={styles.fontStyle}>{this.state.date}</Text>
+                </ListItem>
+              </View>
+              <Content>
+                <List>
+                    { 
+                      this.state.time.map( praytime => 
+                        <ListItem key={praytime.prayerTimeId} thumbnail>
+                          <Left>
+                            <Icon name="md-alarm" style={{color:'black'}} size={30}/>
+                          </Left>
+                          <Body>
+                            <Text style={{color:'black',fontSize:25}}>{praytime.prayerTime}</Text>
+                            <Text style={{fontSize:20}}>{praytime.prayerType}</Text>
+                          </Body>
+                          <Right>
+                            <Switch value={false}/>
+                          </Right>
+                        </ListItem>
+                        )
+                    }
+                </List> 
+              </Content> 
             </View>
-            <Content>
-              <List>
-                  { 
-                    this.state.time.map( praytime => 
-                      <ListItem key={praytime.prayerTimeId} thumbnail>
-                        <Left>
-                          <Icon name="md-alarm" style={{color:'black'}} size={30}/>
-                        </Left>
-                        <Body>
-                          <Text style={{color:'black',fontSize:25}}>{praytime.prayerTime}</Text>
-                          <Text style={{fontSize:20}}>{praytime.prayerType}</Text>
-                        </Body>
-                        <Right>
-                          <Switch value={false}/>
-                        </Right>
-                      </ListItem>
-                      )
-                  }
-              </List> 
-            </Content> 
-          </View>
-            </Content>
-            </Container>
-        );
-      }
-    }
-    
-    const styles = StyleSheet.create({
-        container:{
-            margin : 10
-        },
-        card:{
-          marginTop:10
-        },
-        fontStyle:{
-          fontSize: 25,
-          color: 'black'
+              </Content>
+              </Container>
+          );
         }
-    });
+      }
+      
+      const styles = StyleSheet.create({
+          container:{
+              margin : 10
+          },
+          card:{
+            marginTop:10
+          },
+          fontStyle:{
+            fontSize: 25,
+            color: 'black'
+          }
+      });
