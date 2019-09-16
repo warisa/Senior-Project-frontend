656802760e0ddb1df7573f65bb2d57f014cdc1f0
Warisa <warisa.pae@mail.kmutt.ac.th>
2019-09-16 17:48:24 +0700

    [EDIT] Edit Login button

--- MuslimApp/screen/login.js
+++ MuslimApp/screen/login.js
@@ -2,31 +2,38 @@ import React, { Component } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 import { Container, Title, Button, Icon, Left, Right, Body, Content } from "native-base";
 import Entypo from 'react-native-vector-icons/Entypo';
-import { LoginButton, AccessToken } from 'react-native-fbsdk';
+import { AccessToken, LoginManager  } from 'react-native-fbsdk';
+import Axios from 'axios';
 
 export default class login extends Component {
+  
+  async loginFacebook(){
+    LoginManager.logInWithPermissions(["public_profile","email"]).then(
+      async function(result) {
+        if (result.isCancelled) {
+          console.log("Login cancelled");
+        } else {
+          const accessData = await AccessToken.getCurrentAccessToken();
+          Axios.post('http://10.4.56.94/', { facebookToken: accessData.accessToken })
+          .then( response => {
+          })
+        }
+      },
+      function(error) {
+        console.log("Login fail with error: " + error);
+      }
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Body style={styles.logo}>
             <Image style={{width:250,height:250,marginBottom:20}} source={require('../image/app_logo.png')}/>
-            <LoginButton
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                console.log("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                console.log("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    console.log(data.accessToken.toString())
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => console.log("logout.")}/>
+            <Button style={{marginLeft:20}} onPress={() => {this.loginFacebook()}}>
+              <Entypo name='facebook' style={{color:'white',margin:10}} size={20}/>
+              <Text style={{color:'white',marginRight:10}}>Login with Facebook</Text>
+            </Button>
         </Body>
     </View>
       );
